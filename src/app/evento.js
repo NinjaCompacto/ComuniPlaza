db01ffe46086d6d29fbaf04cd6bd9ff8ce8ebc77
lina-rms <alinerl.eu30@gmail.com>
2024-07-08 15:18:34 -0400

    Correção - Implementação do botão de voltar
    Implementação do botão de voltar para o feed a partir das telas de visualização de evento e vizualização de publicação

--- src/app/evento.js
+++ src/app/evento.js
@@ -1,295 +1,351 @@
-import { 
-    StyleSheet, 
-    Text, 
-    View,
-    TouchableOpacity,
-    Image,
-    SafeAreaView,
-    Alert
-  } from "react-native";
-  import { Ionicons, MaterialIcons } from "@expo/vector-icons";
-  import { useEffect, useState } from "react";
-  import { getEvento, getUser, setUserAttribute } from "../utils/gets";
-  import { userID } from "./(tabs)";
-  import { useGlobalSearchParams } from "expo-router"
-  
-  const EventStatus = ({isFinished}) => { 
-    let color = "#03AA00"
-    let icon = "access-time-filled"
-    let text = "Evento em andamento"
-  
-    if(isFinished){
-      color = "#B80000"
-      icon = "check-circle"
-      text = "Evento finalizado"
-    }
-  
-    return (
-      <View style={{flexDirection: 'row', alignItems: 'center', marginLeft: 28, marginBottom: 10}}>
-        <MaterialIcons name={icon} size={24} color={color}/>
-        <Text style={{color: color, marginLeft: 5, fontWeight: 'bold'}}>{text}</Text>
-      </View>
-      )
-  }
-  
-  const PartcipateButton = ({isParticipating, onPress}) => {
-    let text = "Participar"
-    let backgroundColor = "#03AA00"
-    let width = "80%"
-  
-    if(isParticipating){
-      text = "Participando"
-      backgroundColor = "#0F2355"
-      width = "40%"
-    }
-  
-    return (
-      <TouchableOpacity 
-        style={[styles.submitBtn, {backgroundColor: backgroundColor, width: width}]}
-        onPress={onPress}
-      >
-        <Text style={styles.submitBtnText}>{text}</Text>
-      </TouchableOpacity>
-    )
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  Alert,
+} from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import { useGlobalSearchParams, router } from "expo-router";
+
+import { getEvento, getUser, setUserAttribute } from "../utils/gets";
+import { userID } from "./(tabs)";
+
+// botão de voltar para o feed
+const feedNavigate = () => {
+  router.back();
+};
+
+const EventStatus = ({ isFinished }) => {
+  let color = "#03AA00";
+  let icon = "access-time-filled";
+  let text = "Evento em andamento";
+
+  if (isFinished) {
+    color = "#B80000";
+    icon = "check-circle";
+    text = "Evento finalizado";
   }
 
-async function checkEvents(user, idEvento){
-  if(!("eventosApoiados" in user)){
-    await setUserAttribute(user.doc_id, 'eventosApoiados', [])
-    return false
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        marginLeft: 28,
+        marginBottom: 10,
+      }}
+    >
+      <MaterialIcons name={icon} size={24} color={color} />
+      <Text style={{ color: color, marginLeft: 5, fontWeight: "bold" }}>
+        {text}
+      </Text>
+    </View>
+  );
+};
+
+const PartcipateButton = ({ isParticipating, onPress }) => {
+  let text = "Participar";
+  let backgroundColor = "#03AA00";
+  let width = "80%";
+
+  if (isParticipating) {
+    text = "Participando";
+    backgroundColor = "#0F2355";
+    width = "40%";
   }
 
-  return user.eventosApoiados.includes(idEvento)
+  return (
+    <TouchableOpacity
+      style={[
+        styles.submitBtn,
+        { backgroundColor: backgroundColor, width: width },
+      ]}
+      onPress={onPress}
+    >
+      <Text style={styles.submitBtnText}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+async function checkEvents(user, idEvento) {
+  if (!("eventosApoiados" in user)) {
+    await setUserAttribute(user.doc_id, "eventosApoiados", []);
+    return false;
+  }
+
+  return user.eventosApoiados.includes(idEvento);
 }
 
-  export default function evento() {
-    const [isParticipating, setIsParticipating] = useState(false)
-    const [eventIsFinish, setEventIsFinish] = useState(false)
-    const [render, setRender] = useState(false)
-    const [evento, setEvento] = useState({})
-    const [instituicao, setInstuicao] = useState({})
-    const [user, setUser] = useState({})
-  
-    //const idEvento = "5lYkdSrtGmYUOvr33161enTytDj1_1720053963893"
-    //const idEvento = "UH6R1mtMjDRlJ6fRVPwk0e6XUAm1_1718900467685"
-    const { item } = useGlobalSearchParams()
-    const idEvento = JSON.parse(item).idEvento
-  
-    useEffect(() => {
-      async function fetchDataEvent(idEvento){
-        const fetchedEvento = await getEvento(idEvento)
-        const fetchedInst = await getUser(fetchedEvento.idDono)
-        const fetchedUser = await getUser(userID)
-        const participate = await checkEvents(fetchedUser, idEvento)
+export default function evento() {
+  const [isParticipating, setIsParticipating] = useState(false);
+  const [eventIsFinish, setEventIsFinish] = useState(false);
+  const [render, setRender] = useState(false);
+  const [evento, setEvento] = useState({});
+  const [instituicao, setInstuicao] = useState({});
+  const [user, setUser] = useState({});
 
-        const [day, month, year] = fetchedEvento.finalEvento.split('/')
-        const endDate = new Date(year, month-1, day)
+  //const idEvento = "5lYkdSrtGmYUOvr33161enTytDj1_1720053963893"
+  //const idEvento = "UH6R1mtMjDRlJ6fRVPwk0e6XUAm1_1718900467685"
+  const { item } = useGlobalSearchParams();
+  const idEvento = JSON.parse(item).idEvento;
 
-        setEvento(fetchedEvento)
-        setInstuicao(fetchedInst)
-        setEventIsFinish(new Date() > endDate)
-        setIsParticipating(participate)
-        setUser(fetchedUser)
-        setRender(true)
-      } 
-  
-      fetchDataEvent(idEvento)
-    }, [])
-  
-    return (
-      <SafeAreaView style={styles.container}>
+  useEffect(() => {
+    async function fetchDataEvent(idEvento) {
+      const fetchedEvento = await getEvento(idEvento);
+      const fetchedInst = await getUser(fetchedEvento.idDono);
+      const fetchedUser = await getUser(userID);
+      const participate = await checkEvents(fetchedUser, idEvento);
 
-        <View style={styles.imageContainer}>
-          <Image 
-            source={{ uri: evento.imageUrl }}
-            style={styles.image}
-            alt="imagem do evento"
-          />
+      const [day, month, year] = fetchedEvento.finalEvento.split("/");
+      const endDate = new Date(year, month - 1, day);
+
+      setEvento(fetchedEvento);
+      setInstuicao(fetchedInst);
+      setEventIsFinish(new Date() > endDate);
+      setIsParticipating(participate);
+      setUser(fetchedUser);
+      setRender(true);
+    }
+
+    fetchDataEvent(idEvento);
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.backIcon}>
+        <MaterialIcons
+          name="chevron-left"
+          size={45}
+          color="#0F2355"
+          onPress={feedNavigate}
+        />
+      </View>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{ uri: evento.imageUrl }}
+          style={styles.image}
+          alt="imagem do evento"
+        />
+      </View>
+
+      <View style={styles.eventInfoContainer}>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.eventTitle}>{evento.nomeEvento}</Text>
         </View>
-  
-        <View style={styles.eventInfoContainer}>
-          
-          <View style={{alignItems: 'center'}}>
-            <Text style={styles.eventTitle}>{evento.nomeEvento}</Text>
-          </View>
-  
-          <View style={styles.eventCreatorContainer}>
-            <Ionicons name="person-circle" size={40} color="#7591D9"/>
-            <Text style={styles.creatorName}>{instituicao.nomeCompleto}</Text>
-          </View>
-  
-          <EventStatus isFinished={eventIsFinish}/>
-  
-          {render && !eventIsFinish && <View style={styles.buttonsContainer}>
-            <PartcipateButton 
-              isParticipating={isParticipating} 
-              onPress={async () => {
-                const participateInEvent = await checkEvents(user, idEvento)
-                let eventosApoiados = user.eventosApoiados
 
-                if(!participateInEvent){
-                  eventosApoiados.push(idEvento)
-                  await setUserAttribute(user.doc_id, 'eventosApoiados', eventosApoiados)
-                  user.eventosApoiados = eventosApoiados
-                  setIsParticipating(true)
-                }
-                else{
-                  Alert.alert('Sair do evento', 'Não deseja mais participar do evento?', [
-                    {
-                      text: 'Tirar apoio',
-                      onPress: async () => {
-                        eventosApoiados = eventosApoiados.filter((id) => id !== idEvento)
-                        await setUserAttribute(user.doc_id, 'eventosApoiados', eventosApoiados)  
-                        user.eventosApoiados = eventosApoiados              
-                        setIsParticipating(false)
-                      }
-                    },
-                    {text: 'Continuar apoiando'}
-                  ])
+        <View style={styles.eventCreatorContainer}>
+          <Ionicons name="person-circle" size={40} color="#7591D9" />
+          <Text style={styles.creatorName}>{instituicao.nomeCompleto}</Text>
+        </View>
+
+        <EventStatus isFinished={eventIsFinish} />
+
+        {render && !eventIsFinish && (
+          <View style={styles.buttonsContainer}>
+            <PartcipateButton
+              isParticipating={isParticipating}
+              onPress={async () => {
+                const participateInEvent = await checkEvents(user, idEvento);
+                let eventosApoiados = user.eventosApoiados;
+
+                if (!participateInEvent) {
+                  eventosApoiados.push(idEvento);
+                  await setUserAttribute(
+                    user.doc_id,
+                    "eventosApoiados",
+                    eventosApoiados
+                  );
+                  user.eventosApoiados = eventosApoiados;
+                  setIsParticipating(true);
+                } else {
+                  Alert.alert(
+                    "Sair do evento",
+                    "Não deseja mais participar do evento?",
+                    [
+                      {
+                        text: "Tirar apoio",
+                        onPress: async () => {
+                          eventosApoiados = eventosApoiados.filter(
+                            (id) => id !== idEvento
+                          );
+                          await setUserAttribute(
+                            user.doc_id,
+                            "eventosApoiados",
+                            eventosApoiados
+                          );
+                          user.eventosApoiados = eventosApoiados;
+                          setIsParticipating(false);
+                        },
+                      },
+                      { text: "Continuar apoiando" },
+                    ]
+                  );
                 }
               }}
             />
-  
-            {isParticipating && <TouchableOpacity style={[styles.submitBtn, {flexDirection: 'row', alignItems: 'center', marginLeft: 25}]}>
-              <Ionicons name="chatbox-sharp" size={18} color="#FFF"/>
-              <Text style={styles.submitBtnText}>Chat</Text>
-            </TouchableOpacity>}
-          </View>}
-  
-        </View>
-  
-        <View style={styles.aboutContainer}>
-          <Text style={styles.aboutTitleContainer}>Sobre</Text>
-  
-          <View style={styles.aboutInfoContainer}>
-            <MaterialIcons name="text-snippet" size={22} color="#0F2355" />
-            <Text style={styles.aboutTitle}>Descrição</Text>
-          </View>
-  
-          <Text style={styles.aboutInfoText}>{evento.descricaoEvento}</Text>
-          <View style={{width: '100%', alignItems: 'center'}}><View style={styles.line}/></View>
-          
-          <View style={styles.aboutInfoContainer}>
-            <MaterialIcons name="calendar-month" size={24} color="#0F2355" />
-            <Text style={styles.aboutTitle}>Data de início</Text>
-          </View>
-  
-          <Text style={styles.aboutInfoText}>{evento.inicioEvento}</Text>
-          <View style={{width: '100%', alignItems: 'center'}}><View style={styles.line}/></View>
-          
-          <View style={styles.aboutInfoContainer}>
-            <MaterialIcons name="calendar-month" size={24} color="#0F2355" />
-            <Text style={styles.aboutTitle}>Data de termíno</Text>
-          </View>
-  
-          <Text style={styles.aboutInfoText}>{evento.finalEvento}</Text>
-          <View style={{width: '100%', alignItems: 'center'}}><View style={styles.line}/></View>
-  
-        </View>
-  
-      </SafeAreaView>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-    },
-  
-    imageContainer: {
-      marginTop: 30,
-      height: '30%',
-    },
-  
-    image: {
-      height: '100%',
-      width: '100%',
-      resizeMode: 'contain'
-    },
-  
-    eventInfoContainer: {
-      backgroundColor: '#fff',
-    },
 
-    creatorName: {
-      color: '#0F2355',
-      fontWeight: 'bold'
-    },
-  
-    eventTitle: {
-      color: '#0F2355',
-      fontWeight: 'bold',
-      fontSize: 30,
-      marginTop: 5
-    },
-  
-    eventCreatorContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginLeft: 20,
-    },
-  
-    buttonsContainer: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-  
-    submitBtn: {
-      backgroundColor: "#0F2355",
-      borderRadius: 10,
-      padding: 15,
-      paddingLeft: 5,
-      paddingRight: 5,
-      width: "40%",
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 10,
-      marginBottom: 15
-    },
-  
-    submitBtnText: {
-      color: "#FFF",
-      fontSize: 15,
-      fontWeight: "bold",
-    },
-  
-    aboutContainer: {
-      backgroundColor: '#6A92FA21',  
-      height: '100%'
-    },
-  
-    aboutTitleContainer: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: '#0F2355',
-      marginLeft: 20,
-      marginTop: 10,
-      marginBottom: 5
-    },
-  
-    aboutInfoContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginLeft: 25
-    },
-  
-    aboutTitle: {
-      color: '#0F2355',
-      fontWeight: 'bold'
-    },
-  
-    aboutInfoText: {
-      marginLeft: 40,
-      color: '#0F2355',
-    },
-  
-    line: {
-      width: '90%',
-      backgroundColor: '#0F2355',
-      height: 1,
-      marginTop: 7,
-      marginBottom: 7
-    }
-  })
+            {isParticipating && (
+              <TouchableOpacity
+                style={[
+                  styles.submitBtn,
+                  {
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginLeft: 25,
+                  },
+                ]}
+              >
+                <Ionicons name="chatbox-sharp" size={18} color="#FFF" />
+                <Text style={styles.submitBtnText}>Chat</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        )}
+      </View>
+
+      <View style={styles.aboutContainer}>
+        <Text style={styles.aboutTitleContainer}>Sobre</Text>
+
+        <View style={styles.aboutInfoContainer}>
+          <MaterialIcons name="text-snippet" size={22} color="#0F2355" />
+          <Text style={styles.aboutTitle}>Descrição</Text>
+        </View>
+
+        <Text style={styles.aboutInfoText}>{evento.descricaoEvento}</Text>
+        <View style={{ width: "100%", alignItems: "center" }}>
+          <View style={styles.line} />
+        </View>
+
+        <View style={styles.aboutInfoContainer}>
+          <MaterialIcons name="calendar-month" size={24} color="#0F2355" />
+          <Text style={styles.aboutTitle}>Data de início</Text>
+        </View>
+
+        <Text style={styles.aboutInfoText}>{evento.inicioEvento}</Text>
+        <View style={{ width: "100%", alignItems: "center" }}>
+          <View style={styles.line} />
+        </View>
+
+        <View style={styles.aboutInfoContainer}>
+          <MaterialIcons name="calendar-month" size={24} color="#0F2355" />
+          <Text style={styles.aboutTitle}>Data de termíno</Text>
+        </View>
+
+        <Text style={styles.aboutInfoText}>{evento.finalEvento}</Text>
+        <View style={{ width: "100%", alignItems: "center" }}>
+          <View style={styles.line} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  backIcon: {
+    marginLeft: 6,
+    marginTop: 25,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+
+  imageContainer: {
+    height: "30%",
+  },
+
+  image: {
+    height: "100%",
+    width: "100%",
+    resizeMode: "contain",
+  },
+
+  eventInfoContainer: {
+    backgroundColor: "#fff",
+  },
+
+  creatorName: {
+    color: "#0F2355",
+    fontWeight: "bold",
+  },
+
+  eventTitle: {
+    color: "#0F2355",
+    fontWeight: "bold",
+    fontSize: 30,
+    marginTop: 5,
+  },
+
+  eventCreatorContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 20,
+  },
+
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  submitBtn: {
+    backgroundColor: "#0F2355",
+    borderRadius: 10,
+    padding: 15,
+    paddingLeft: 5,
+    paddingRight: 5,
+    width: "40%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 15,
+  },
+
+  submitBtnText: {
+    color: "#FFF",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+
+  aboutContainer: {
+    backgroundColor: "#6A92FA21",
+    height: "100%",
+  },
+
+  aboutTitleContainer: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#0F2355",
+    marginLeft: 20,
+    marginTop: 10,
+    marginBottom: 5,
+  },
+
+  aboutInfoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 25,
+  },
+
+  aboutTitle: {
+    color: "#0F2355",
+    fontWeight: "bold",
+  },
+
+  aboutInfoText: {
+    marginLeft: 40,
+    color: "#0F2355",
+  },
+
+  line: {
+    width: "90%",
+    backgroundColor: "#0F2355",
+    height: 1,
+    marginTop: 7,
+    marginBottom: 7,
+  },
+});
